2dc94961fb179141612ef2d9a8c570a537a1da1d
Schmelzberry <jelsberry96@gmail.com>
2023-08-20 17:45:36 -0700

    display results to page, only 1 submission works atm

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business Logic
 function count(input) {
   let splitInput = input.split(" ");
   let userInfoArray = [];
@@ -25,3 +26,37 @@ function numReplace(userInfoArray) {
   });
   return filteredArray;
 }
+
+// UI Logic
+function formSubmission(event) {
+  let form = document.querySelector("form")
+  let output =  document.getElementById("bot-output")
+  event.preventDefault();
+  const numsToReplace = document.getElementById("bot").value;
+  const numsFiltered = count(numsToReplace);
+  document.getElementById("bot").innerText = count;
+  output.append("Beepity Boopity, here's your results: " + numsFiltered);
+  form.reset();
+}
+
+function refreshFormResults() {
+  if (document.querySelector("input").innerText !== "") {
+    console.log("it works")
+  document.getElementById("bot-output").innerText = "";
+  }
+ 
+}
+
+
+// function clickHappened() {
+//   let button = document.querySelector("button");
+//   button.addEventListener("click", refreshFormResults);
+// }
+
+
+window.addEventListener("load", function() {
+  this.document.querySelector("form").addEventListener("submit", formSubmission);
+})
+
+
+
